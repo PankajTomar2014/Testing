695680318d83c093caf698284dc6243a41afc4c6
pankajtomar <mobileprogramming@techleads-Mac-mini-2.local>
2023-10-25 17:27:56 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+
+# Jest
+Jest is the most commonly used library for unit testing and component testing in React Native. It's the default choice in many React Native projects and works well for testing JavaScript logic, components, and reducers.
+
+yarn add --dev jest ( Installed as dev Dependency )
+
+
+# Testing Folder 
+    Unit : This directory is for unit tests. Each component or module in your src folder should have a corresponding test file in this directory.
+
+    Integration : This directory is for integration tests. Integration tests can test the interaction between different parts of your application, such as testing how screens and components work together.
+
+    setupTests.js: This file is used to set up the testing environment. You can configure testing libraries and add global configurations here.
+
---
+++ Src/Components/Button.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import {StyleSheet,Image, TextInput,View, Text, TouchableOpacity} from 'react-native';
+
+export const CustomeButton = props => {
+  const {btnText, onPress,style} = props;
+  return (
+    <TouchableOpacity onPress={onPress} style={[styles.touchStyle,style]}>
+      <Text style={styles.touchText}>{btnText}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export const Input = props => {
+  const { isPassword } = props
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput style={styles.input} {...props} />
+      {isPassword && <HidePassword {...props}/>}
+    </View>
+  );
+};
+
+export const HidePassword = props => {
+  const { onPress ,isHide } = props;
+  return (
+    <TouchableOpacity 
+    onPress={onPress}
+    style={styles.hideContainer}>
+        <Image
+          style={{height:30,width:30}}
+          resizeMode={'contain'}
+          source={
+            isHide ? {uri: 'https://cdn-icons-png.flaticon.com/128/2710/2710718.png'}
+            : {uri:'https://cdn-icons-png.flaticon.com/128/11502/11502541.png'}
+          }
+        />
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  touchStyle: {
+    alignSelf: 'center',
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 50,
+    width: '100%',
+    backgroundColor: '#0841A0',
+  },
+  touchText: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: 'white',
+  },
+  inputContainer:{
+    flexDirection:'row',    
+    borderWidth:1,
+    borderRadius:5,
+    marginVertical:5,
+    alignItems:"center",
+    height: 50,
+    width: '100%',
+    alignSelf:"center",
+    justifyContent:"space-between"
+  },
+  input: {
+    paddingLeft: 20,
+  },
+  hideContainer:{   
+    alignSelf:"flex-end",
+    height:'100%',
+    justifyContent:"center",
+    alignItems:"center",
+    width:'20%'
+  }
+});
